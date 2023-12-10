a3b90efa36e2609a30a3e5adb622886dfacd4e87
DielliHana <diellona.shefkiu@gmail.com>
2023-12-10 17:31:31 +0100

    js

--- Greta1.js
+++ Greta1.js
@@ -6,3 +6,23 @@ function topNav(){
         x.className ="topNav";
     }
 }
+document.getElementById('menuButton').addEventListener('click', function(){
+    document.getElementById('menu').style.display='block';
+});
+document.getElementById('contactButton').addEventListener('click', function(){
+    document.getElementById('contact').style.display='block';
+});
+document.querySelector('#menu b-button c-display-topright large').addEventListener('click', function(){
+    document.getElementById('menu').style.display='none';
+});
+document.querySelector('#contact b-button c-display-topright l-large').addEventListener('click', function(){
+    document.getElementById('contact').style.display='none';
+});
+document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    alert('Form submitted succesfully!');
+    this.reset();
+
+    document.getElementById('contact').style.display='none';
+});
