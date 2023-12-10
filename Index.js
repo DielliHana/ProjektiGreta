878044dfa8f62cc03e4291b6683a3d4230aeb380
DielliHana <diellona.shefkiu@gmail.com>
2023-12-10 23:53:36 +0100

    validiim

---
+++ Index.js
@@ -0,0 +1,13 @@
+function regjistrimi(){
+            
+    var Name = document.getElementById('Name').value;   
+    var People = document.getElementById('People').value;   
+    var date = document.getElementById('date').value;   
+    var Message = document.getElementById('Message').value;
+
+if(!Name || !People || !date || Message){
+    alert('All fields must be filled!');
+    return;
+}else{
+    alert('Registration ended succesfully!');
+}
